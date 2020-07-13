749bcd28d5ef6090bb4115aa30036532b7f85158
Singhdivya4444 <68139013+Singhdivya4444@users.noreply.github.com>
2020-07-13 17:13:52 +0530

    Add files via upload

---
+++ assigmentdiff.c
@@ -0,0 +1,12 @@
+
+#include<stdio.h>
+int main()
+{
+
+
+ int a,b;
+ printf("enter value for a and b\n");
+ scanf("%d %d", &a, &b);
+ printf("subtraction of %d and %d is %d\n",(a-b));
+ return 0;
+}
