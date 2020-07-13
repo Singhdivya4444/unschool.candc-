dad6650915dd377c9a190016b6baa9a0c1f63b03
Singhdivya4444 <68139013+Singhdivya4444@users.noreply.github.com>
2020-07-13 17:15:01 +0530

    Add files via upload

---
+++ assigmentsum3no.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("enter value for a ,b and c\n");
+    scanf("%d %d %d", &a,&b, &c);
+    printf("addition of %d , %d and %d is %d\n",a, b, c, (a+b+c));
+    return 0;
+}
