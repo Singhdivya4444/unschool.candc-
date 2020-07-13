8a3a8b98e840176f84a949e4540af3486028cf8c
Singhdivya4444 <68139013+Singhdivya4444@users.noreply.github.com>
2020-07-13 17:17:28 +0530

    Add files via upload

---
+++ assignment.c
@@ -0,0 +1,12 @@
+# include<stdio.h>
+void main()
+{
+    int a,b,c;
+    printf("enter a first no");
+    scanf("%d",&a);
+    printf("enter a second no");
+    scanf("%d",&b);
+    c=a+b;
+    printf("addition of no is =%d",c);
+    getch();
+}
