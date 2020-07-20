53a7f5085a429cc57e5a2e84a3808ad65e8ee7fb
Singhdivya4444 <68139013+Singhdivya4444@users.noreply.github.com>
2020-07-20 17:57:45 +0530

    Add files via upload

---
+++ day.c
@@ -0,0 +1,38 @@
+   #include <stdio.h>
+   int main (void)
+   {
+       int w,day,k,j,month,year,m,d;
+       printf("enter the day:\n");
+       scanf("%d",&d);
+       printf("enter the month:\n");
+       scanf("%d",&m);
+       printf("enter the year:\n");
+       scanf("%d",&year);
+       j=year/100;
+       k=year%100;
+       w=d+(13*m+13)/5;
+       w=w+k+k/4+j/4;
+       w=w-2*j;
+       w=w%7;
+       if(w==0)
+        printf("\nsaturday");
+       else if
+        (w==1)
+        printf("\nsunday");
+       else if
+        (w==2)
+        printf("\nmonday");
+       else if
+        (w==3)
+        printf("\ntuesday");
+       else if
+        (w==4)
+        printf("\nwednesday");
+       else if
+        (w==5)
+        printf("\nthursday");
+       else if
+        (w==6)
+        printf("\nfriday");
+       return 0;
+   }
