eee80333d9f25fcba3d25c57c2e70befad1c8b25
Singhdivya4444 <68139013+Singhdivya4444@users.noreply.github.com>
2020-08-01 10:11:38 +0530

    Add files via upload

---
+++ strrev.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str[50]="rashii";
+    printf("the given string is=%s\n",str);
+    printf("after reversing string is=%s",strrev(str));
+    return 0;
+}
